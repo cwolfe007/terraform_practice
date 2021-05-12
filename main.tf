65c3cff8bdfc08054e91ac290dc7435c59d66154
Caleb Wolfe <cwolfe651@gmail.com>
2021-05-12 11:08:01 -0500

    adding variable

--- main.tf
+++ main.tf
@@ -9,15 +9,22 @@ terraform {
   required_version = ">=0.14.9"
 }
 
+variable "instance_name" {
+    description = "Value of the name of the EC2 instance"
+    type = string
+    default = "GETRICKITYWRECKEDSON"
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0d5eff06f840b45e9"
+  ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
   tags = {
-    "Name" = "CalebsTestTFInstance"
+    "Name" = var.instance_name
+    "ANewTag" = "wow such change"
   }
 }
